8affd97d114ccd93f3cbdce80927332d5bd5782b
aluno pos <alunopos@local.inatel.br>
2019-11-23 16:15:00 -0300

    Implement HTTP POST

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+const TaskService = require('../services/taskService');
+
 const checkAuth = require('../middleware/check-auth');
 
-router.post('/', checkAuth, (request, response) => {
-  response.status(200).json({
-    message: 'Handling POST requests to /api/tasks'
-  });
+router.post('/', checkAuth, async (request, response) => {
+
+  const task = await TaskService.add(request.body);
+
+  response.status(200).json(task);
 });
 
 router.get('/', (request, response) => {
